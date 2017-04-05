03d2f80f6ac82677f2cd9fff0068e5e7989b0cc8
Patrick <porchewellness@gmail.com>
2017-04-05 13:44:33 -0700

    added reactUi front-end to browser-games

---
+++ reactUi/user-interface/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Announcement from './Announcement';
+import ResetButton from './ResetButton';
+import Tile from './Tile';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      gameBoard: [
+        "", "", "",
+        "", "", "",
+        "", "", ""
+      ],
+      turn: "X"
+    }
+  }
+  updateBoard(loc, player){
+    
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="menu">
+          <h1>Tic-Tac-Toe</h1>
+          <Announcement />
+          <ResetButton />
+        </div>
+        {this.state.gameBoard.map(function(value, i){
+          return (
+            <Tile
+              key={i}
+              loc={i}
+              value={value}
+              updateBoard={this.updateBoard.bind(this)}
+              turn={this.state.turn} />
+          )
+        }.bind(this))}
+      </div>
+    );
+  }
+}
+
+export default App;
