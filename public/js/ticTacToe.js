99a89973de15fe5f3c444b19bd3db9fc2d5912e2
Patrick <porchewellness@gmail.com>
2017-04-04 23:16:19 -0700

    tested code to see what was working have it running without errors worked on playerTurn function

--- public/js/ticTacToe.js
+++ public/js/ticTacToe.js
@@ -1,15 +1,17 @@
+var gameBoard = document.getElementById("gameBoard");
 var gameboardArray = ["#","#","#","#","#","#","#","#","#"];
 var winConditionData = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 var gameHasWinner = false;
 var resetGame = false;
+var counter = 0;
 var player = {
-  symbol = "#",
+  symbol: "#",
   class: "#"
-}
+};
 var computer = {
-  symbol = "#",
+  symbol: "#",
   class: "#"
-}
+};
 var turnCounter = 0;
 
 function setPlayerXorO(){
@@ -17,21 +19,48 @@ function setPlayerXorO(){
   if(player.symbol === "X"){
     computer.symbol = "O";
     computer.class = "fa fa-opera";
-    player.class = "fa fa-times"
+    player.class = "fa fa-times";
   } else {
     computer.symbol = "X";
-    computer.class = "fa fa-times"
+    computer.class = "fa fa-times";
     player.symbol = "O";
-    player.class = "fa fa-opera"
+    player.class = "fa fa-opera";
   }
 }
 
-function playerTurn(){
-var playerSquarePosition = dataset.gameBoard.location;
-gameboardArray[playerSquarePosition] = player.symbol;
-checkGameStatusHelper();
-computerTurn();
+// function playerTurn(){
+// var playerSquarePosition = parseInt(dataset.gameBoard.location);
+// gameboardArray[playerSquarePosition] = player.symbol;
+
+var square0 = document.getElementById("0");
+var square1 = document.getElementById("1");
+var square2 = document.getElementById("2");
+var square3 = document.getElementById("3");
+var square4 = document.getElementById("4");
+var square5 = document.getElementById("5");
+var square6 = document.getElementById("6");
+var square7 = document.getElementById("7");
+var square8 = document.getElementById("8");
+
+function playerTurn(square0, square1, square3, square4, square5, square6, square7, square8){
+  var square0 = document.getElementById("0");
+  var square1 = document.getElementById("1");
+  var square2 = document.getElementById("2");
+  var square3 = document.getElementById("3");
+  var square4 = document.getElementById("4");
+  var square5 = document.getElementById("5");
+  var square6 = document.getElementById("6");
+  var square7 = document.getElementById("7");
+  var square8 = document.getElementById("8");
+  if(gameboardArray[0] === "#"){
+    square0.className += player.class;
+
+  }
 }
+
+// checkGameStatusHelper();
+// computerTurn();
+
 
 function computerTurn(){
 var computerSquarePosition = (Math.random()*10).toFixed();
@@ -45,7 +74,7 @@ checkGameStatusHelper();
 
 function updateArray(gameBoardLocation, playerOrComp){
   if(gameboardArray[gameBoardLocation] === "#"){
-    gameboardArray[gameBoardLocation] = playerOrComp
+    gameboardArray[gameBoardLocation] = playerOrComp;
   } else {
     computerTurn();
   }
@@ -53,6 +82,7 @@ function updateArray(gameBoardLocation, playerOrComp){
 
 function declareDraw(){
   if(counter === 9){
+    promt("There is no winner");
     resetGame = true;
   }
 }
@@ -65,20 +95,22 @@ function checkWinner(winConditionData){
 
 function checkWinnerHelper(index1, index2, index3){
     if(gameboardArray[index1] === "X" && gameboardArray[index2] === "X" && gameboardArray[index3] === "X"){
+      prompt("X's Win!");
       gameHasWinner = true;
       resetGame = true;
     } else if(gameboardArray[index1] === "O" && gameboardArray[index2] === "O" && gameboardArray[index3] === "O") {
+      prompt("O's win!");
       gameHasWinner = true;
       resetGame = true;
     }
 }
 
-function checkGameStatusHelper(){
-  counter++;
-  updateArray();
-  checkWinner();
-  declareDraw();
-}
+// function checkGameStatusHelper(){
+//   counter++;
+//   updateArray();
+//   checkWinner();
+//   declareDraw();
+// }
 
 function resetGame(){
   if(resetGame === true){
